2aa2ecb38e0ef3999ce32dcc4acbc955f86c27e7
Prerak Stha <prerakshrestha2018@gmail.com>
2026-09-15 20:39:52 +0545

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT 
+
+console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () => console.log('Server is running on port', PORT));
