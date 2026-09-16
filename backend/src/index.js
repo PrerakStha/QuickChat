8ddfa83a3f22a087e70291b2b5c77db2a59f2ca3
Prerak Stha <prerakshrestha2018@gmail.com>
2026-09-16 18:32:58 +0545

    Add Dockerfile for multi-stage build and create .dockerignore

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,8 @@ import express from "express";
 import cors from "cors";
 
 import "dotenv/config";
+import fs from "fs";
+import path from "path";
 
 import { clerkMiddleware } from '@clerk/express'
 
@@ -19,12 +21,20 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3001";
 console.log(process.env.MONGO_URI);
 
 app.use(express.json());
-app.use(cors({ origin: FRONTEND_URL,credentials: true }));
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
     res.status(200).json({ status: "ok" });
 });
+
+
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir))
+    app.get("/{*any}",(req,res,next)=>{
+        res.sendFile(path.join(publicDir, "index.html"),(err)=>next(err));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
