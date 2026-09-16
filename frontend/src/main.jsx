9222256e16924b0489ab86f25ce5677fc4666be9
Prerak Stha <prerakshrestha2018@gmail.com>
2026-09-16 19:27:13 +0545

    chore: update .dockerignore and fix build script formatting; enhance ClerkProvider integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
 
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ClerkProvider>
+  <StrictMode >
+  <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
     <App />
     </ClerkProvider>
   </StrictMode>,
